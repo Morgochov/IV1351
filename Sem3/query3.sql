5fccc277bd6815d23c02dfe6b18fbb1534cf377a
CAndreasW <62574715+CAndreasW@users.noreply.github.com>
2023-12-04 17:39:44 +0100

    Update query3.sql

--- Sem3/query3.sql
+++ Sem3/query3.sql
@@ -4,4 +4,4 @@ JOIN lesson l ON i.instructor_ID = l.instructor_ID
 JOIN person p ON i.person_number = p.person_number
 WHERE EXTRACT(MONTH FROM l.time) = EXTRACT(MONTH FROM CURRENT_DATE)
 GROUP BY i.instructor_ID, p.name
-HAVING COUNT(*) > 0; -- Replace 0 with your specific number
+HAVING COUNT(*) > 1; -- Replace 1 with your specific number of lessons
