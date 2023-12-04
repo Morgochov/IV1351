94796c94c755a89b2a10eda4ec383b5df5ec855d
CAndreasW <62574715+CAndreasW@users.noreply.github.com>
2023-12-04 17:43:17 +0100

    Update insertion.sql

--- Sem3/insertion.sql
+++ Sem3/insertion.sql
@@ -82,7 +82,7 @@ INSERT INTO instructor_schedule (instructor_ID, date) VALUES
 INSERT INTO lesson (lesson_ID, instructor_ID, time, place, type, proficiency, spots, minimum_spots) VALUES
 (1, 1, '2023-12-6 10:00:00', 'Studio A', 'Individual', 'Beginner', '5', '3'),
 (2, 2, '2023-12-7 14:00:00', 'Room B', 'Group', 'Intermediate', '10', '8'),
-(3, 3, '2023-12-8 16:30:00', 'Hall C', 'Ensemble', 'Beginner', '1', '1');
+(3, 3, '2023-12-8 16:30:00', 'Hall C', 'Ensemble', 'Beginner', '1', '1'),
 (4, 3, '2023-12-9 16:30:00', 'Hall C', 'Ensemble', 'Advanced', '15', '10'),
 (5, 3, '2023-12-10 16:30:00', 'Hall C', 'Ensemble', 'Intermediate', '2', '1');
 
