867361e286c4a92632649f093be1cb2edbe89e3f
CAndreasW <62574715+CAndreasW@users.noreply.github.com>
2023-12-04 17:02:11 +0100

    Update query3.sql

--- Sem3/query3.sql
+++ Sem3/query3.sql
@@ -1,7 +1,7 @@
-SELECT i.instructor_ID, p.name, COUNT() AS lesson_count
+SELECT i.instructor_ID, p.name, COUNT(*) AS lesson_count
 FROM instructor i
 JOIN lesson l ON i.instructor_ID = l.instructor_ID
 JOIN person p ON i.person_number = p.person_number
 WHERE EXTRACT(MONTH FROM l.time) = EXTRACT(MONTH FROM CURRENT_DATE)
 GROUP BY i.instructor_ID, p.name
-HAVING COUNT() > 0; -- Replace 0 with your specific number
+HAVING COUNT(*) > 0; -- Replace 0 with your specific number
